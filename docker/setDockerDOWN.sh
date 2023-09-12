64c4fa1756c66608788def220fa9e9dd7dcd25f6
FerdiStro <ferdistr08@gmail.com>
2023-09-12 22:09:39 +0200

    Add Proxy x

--- docker/setDockerDOWN.sh
+++ docker/setDockerDOWN.sh
@@ -1,3 +1,4 @@
 echo "Removing Docker Services"
 docker stack rm DNS
+docker stack rm PROXY
 docker stack rm nextcloud
--- docker/setDockerUP.sh
+++ docker/setDockerUP.sh
@@ -3,5 +3,8 @@ cd DNS/
 docker stack deploy -c docker-compose.yml DNS
 cd ../nextcloud
 docker stack deploy -c docker-compose.yml nextcloud
+cd ../PROXY
+docker stack deploy -c docker-compose.yml PROXY
+
 
 watch docker service ls
